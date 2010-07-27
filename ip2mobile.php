306e0f44b9a820b1c26764a280dcfba12709f0c2
tasuku <tasuku@d0d07461-0603-4401-acd4-de1884942a52>
2010-07-27 04:52:45 +0000

    add ip2mobile.php generated by scrape.py.
    git-svn-id: http://svn.coderepos.org/share/lang/python/cidr-mobilejp/trunk@38027 d0d07461-0603-4401-acd4-de1884942a52

---
+++ ip2mobile.php
@@ -0,0 +1,299 @@
+<?php
+function ip2mobile($ip) {
+  $n = sprintf('%u', ip2long($ip));
+
+  if ($n < 1913929984) {
+    if ($n < 1867944704) {
+      if ($n < 1036427264) {
+        if ($n < 1036419072) {
+          if ($n < 1031078144) {
+            if ($n >= 998712960 && $n <= 998713087) {
+              return 'ezweb';
+            }
+          } else if ($n <= 1031078159) {
+            return 'ezweb';
+          } else {
+            if ($n >= 1031078432 && $n <= 1031078447) {
+              return 'ezweb';
+            }
+          }
+        } else if ($n <= 1036421631) {
+          return 'airhphone';
+        } else {
+          if ($n < 1036421888) {
+            if ($n >= 1036421732 && $n <= 1036421735) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1036421895) {
+            return 'airhphone';
+          } else {
+            if ($n < 1036422016) {
+            } else if ($n <= 1036422063) {
+              return 'airhphone';
+            } else {
+              if ($n >= 1036422144 && $n <= 1036423167) {
+                return 'airhphone';
+              }
+            }
+          }
+        }
+      } else if ($n <= 1036429055) {
+        return 'airhphone';
+      } else {
+        if ($n < 1036780032) {
+          if ($n < 1036449792) {
+            if ($n >= 1036429312 && $n <= 1036431359) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1036451839) {
+            return 'airhphone';
+          } else {
+            if ($n >= 1036779520 && $n <= 1036779775) {
+              return 'airhphone';
+            }
+          }
+        } else if ($n <= 1036781439) {
+          return 'airhphone';
+        } else {
+          if ($n < 1867943232) {
+            if ($n >= 1036803072 && $n <= 1036804095) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1867943487) {
+            return 'ezweb';
+          } else {
+            if ($n < 1867943552) {
+            } else if ($n <= 1867943743) {
+              return 'ezweb';
+            } else {
+              if ($n >= 1867943872 && $n <= 1867943935) {
+                return 'ezweb';
+              }
+            }
+          }
+        }
+      }
+    } else if ($n <= 1867944735) {
+      return 'ezweb';
+    } else {
+      if ($n < 1913927680) {
+        if ($n < 1913926144) {
+          if ($n < 1913925888) {
+            if ($n >= 1868151808 && $n <= 1868152831) {
+              return 'docomo';
+            }
+          } else if ($n <= 1913926143) {
+            return 'airhphone';
+          } else {
+            if ($n >= 1913925888 && $n <= 1913926399) {
+              return 'airhphone';
+            }
+          }
+        } else if ($n <= 1913926655) {
+          return 'airhphone';
+        } else {
+          if ($n < 1913926656) {
+            if ($n >= 1913926400 && $n <= 1913926911) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1913927423) {
+            return 'airhphone';
+          } else {
+            if ($n < 1913927168) {
+            } else if ($n <= 1913927679) {
+              return 'airhphone';
+            } else {
+              if ($n >= 1913927424 && $n <= 1913927935) {
+                return 'airhphone';
+              }
+            }
+          }
+        }
+      } else if ($n <= 1913928191) {
+        return 'airhphone';
+      } else {
+        if ($n < 1913928704) {
+          if ($n < 1913928192) {
+            if ($n >= 1913927936 && $n <= 1913928447) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1913928703) {
+            return 'airhphone';
+          } else {
+            if ($n >= 1913928448 && $n <= 1913928959) {
+              return 'airhphone';
+            }
+          }
+        } else if ($n <= 1913929215) {
+          return 'airhphone';
+        } else {
+          if ($n < 1913929216) {
+            if ($n >= 1913928960 && $n <= 1913929471) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1913929727) {
+            return 'airhphone';
+          } else {
+            if ($n < 1913929472) {
+            } else if ($n <= 1913929983) {
+              return 'airhphone';
+            } else {
+              if ($n >= 1913929728 && $n <= 1913930239) {
+                return 'airhphone';
+              }
+            }
+          }
+        }
+      }
+    }
+  } else if ($n <= 1913930495) {
+    return 'airhphone';
+  } else {
+    if ($n < 3414870784) {
+      if ($n < 1990165952) {
+        if ($n < 1914044160) {
+          if ($n < 1913930496) {
+            if ($n >= 1913930240 && $n <= 1913930751) {
+              return 'airhphone';
+            }
+          } else if ($n <= 1913930751) {
+            return 'airhphone';
+          } else {
+            if ($n >= 1914044160 && $n <= 1914044191) {
+              return 'airhphone';
+            }
+          }
+        } else if ($n <= 1914044191) {
+          return 'airhphone';
+        } else {
+          if ($n < 1990165248) {
+            if ($n >= 1989727936 && $n <= 1989727999) {
+              return 'ezweb';
+            }
+          } else if ($n <= 1990165375) {
+            return 'ezweb';
+          } else {
+            if ($n < 1990165664) {
+            } else if ($n <= 1990165695) {
+              return 'ezweb';
+            } else {
+              if ($n >= 1990165760 && $n <= 1990165887) {
+                return 'ezweb';
+              }
+            }
+          }
+        }
+      } else if ($n <= 1990166015) {
+        return 'ezweb';
+      } else {
+        if ($n < 2089987584) {
+          if ($n < 2037375904) {
+            if ($n >= 2037375744 && $n <= 2037375871) {
+              return 'ezweb';
+            }
+          } else if ($n <= 2037375935) {
+            return 'ezweb';
+          } else {
+            if ($n >= 2037376768 && $n <= 2037376895) {
+              return 'ezweb';
+            }
+          }
+        } else if ($n <= 2089988095) {
+          return 'docomo';
+        } else {
+          if ($n < 2098989824) {
+            if ($n >= 2098987008 && $n <= 2098989311) {
+              return 'airhphone';
+            }
+          } else if ($n <= 2098991615) {
+            return 'airhphone';
+          } else {
+            if ($n < 3404050432) {
+            } else if ($n <= 3404051455) {
+              return 'docomo';
+            } else {
+              if ($n >= 3414864896 && $n <= 3414865407) {
+                return 'docomo';
+              }
+            }
+          }
+        }
+      }
+    } else if ($n <= 3414871039) {
+      return 'docomo';
+    } else {
+      if ($n < 3681288704) {
+        if ($n < 3534288384) {
+          if ($n < 3533263872) {
+            if ($n >= 3532169472 && $n <= 3532169727) {
+              return 'docomo';
+            }
+          } else if ($n <= 3533264127) {
+            return 'docomo';
+          } else {
+            if ($n >= 3533264384 && $n <= 3533264895) {
+              return 'docomo';
+            }
+          }
+        } else if ($n <= 3534288895) {
+          return 'airhphone';
+        } else {
+          if ($n < 3538321632) {
+            if ($n >= 3534314496 && $n <= 3534316543) {
+              return 'airhphone';
+            }
+          } else if ($n <= 3538321647) {
+            return 'ezweb';
+          } else {
+            if ($n < 3541231616) {
+            } else if ($n <= 3541233663) {
+              return 'airhphone';
+            } else {
+              if ($n >= 3548299392 && $n <= 3548299519) {
+                return 'airhphone';
+              }
+            }
+          }
+        }
+      } else if ($n <= 3681292287) {
+        return 'airhphone';
+      } else {
+        if ($n < 3682439424) {
+          if ($n < 3681328640) {
+            if ($n >= 3681328384 && $n <= 3681328511) {
+              return 'ezweb';
+            }
+          } else if ($n <= 3681328671) {
+            return 'ezweb';
+          } else {
+            if ($n >= 3681328680 && $n <= 3681328687) {
+              return 'ezweb';
+            }
+          }
+        } else if ($n <= 3682439551) {
+          return 'ezweb';
+        } else {
+          if ($n < 3682440192) {
+            if ($n >= 3682439680 && $n <= 3682439695) {
+              return 'ezweb';
+            }
+          } else if ($n <= 3682440319) {
+            return 'ezweb';
+          } else {
+            if ($n < 3715563520) {
+            } else if ($n <= 3715566079) {
+              return 'airhphone';
+            } else {
+              if ($n >= 3724885632 && $n <= 3724886015) {
+                return 'ezweb';
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return 'pc';
+}
+?>
+
